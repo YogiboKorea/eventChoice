9b03fa1889d13b53cc981e9aab5fef5407a4b02d
YogiboKorea <yogico.design@gmail.com>
2024-09-24 11:33:55 +0900

    UP

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();  // .env 파일 불러오기
@@ -7,11 +9,12 @@ require('dotenv').config();  // .env 파일 불러오기
 app.use(bodyParser.json());
 const PORT = 4000;
 const mongoUri = process.env.MONGODB_URI;  // .env 파일의 MONGODB_URI 사용
-
-/// MongoDB 연결
 MongoClient.connect(mongoUri, function (err, client) {
     if (err) {
         console.error('Failed to connect to MongoDB:', err);
+        app.use((req, res) => {
+            res.status(500).json({ error: 'Failed to connect to the database' });
+        });
         return;
     }
 
@@ -23,6 +26,11 @@ MongoClient.connect(mongoUri, function (err, client) {
     // API: 회원 참여 여부 확인
     app.post('/checkParticipation', function (req, res) {
         const memberId = req.body.memberId;
+
+        if (!memberId) {
+            res.status(400).json({ error: 'memberId is required' });
+            return;
+        }
 
         // MongoDB에서 회원 ID 검색
         collection.findOne({ memberId: memberId }, function (err, result) {
